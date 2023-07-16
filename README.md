debb019cce110976fcb7a308989ae48d0dc6989a
RocketGod <57732082+RocketGod-git@users.noreply.github.com>
2023-07-16 11:41:46 -0700

    Create ADS-B message streamer

---
+++ README.md
@@ -0,0 +1,59 @@
+# ADS-B Aircraft Tracking with Discord Notifications
+
+This project provides a script to track aircraft using Automatic Dependent Surveillance-Broadcast (ADS-B) data, and sends notifications to a Discord server via a webhook when new aircraft are detected.
+
+## Prerequisites
+
+- Python 3.7 or above
+- Access to a source of ADS-B data, such as an SDR receiver like HackRF, RTL-SDR, SDRangel, or a log file with ADS-B messages.
+
+## Dependencies
+
+This project relies on a few Python libraries:
+- `requests` for sending HTTP requests to the Discord webhook
+- `pyModeS` for decoding ADS-B messages
+- `colorama` for colored console output
+
+To install these dependencies, run:
+
+```bash
+pip install requests pyModeS colorama
+```
+
+## Usage
+
+1. Clone this repository or download the script `adsb-streamer.py`.
+
+2. Update the `WEBHOOK_URL` variable in the script with your Discord webhook URL.
+
+3. Update the `FILENAME` variable in the script with the path to your ADS-B data file.
+
+The ADS-B data file should have lines in the following format:
+
+```
+Sun Jul 16 2023,11:31:40,5da9636cfa39ce25854eff84feda,13.2818
+```
+
+This includes a timestamp, the ADS-B message, and a signal strength value, separated by commas. This script currently uses only the timestamp and ADS-B message.
+
+4. Run the script:
+
+```bash
+python adsb_streamer.py
+```
+
+The script will continuously read from the ADS-B data file, decode any aircraft information it can from the ADS-B messages, stream it in the terminal window, and send a notification to the specified Discord webhook whenever a new aircraft is detected.
+
+## Notes
+
+This script is designed to be robust and to handle various errors gracefully. If an error occurs during the reading or processing of lines from the file, the script will log the error, pause for 1 second, and then continue reading and processing lines. The script can be stopped by sending a `KeyboardInterrupt` (e.g., by pressing `Ctrl+C`).
+
+## Using SDRangel to Generate ADS-B Data
+
+SDRangel is a popular software-defined radio (SDR) receiver that can be used to receive ADS-B messages from aircraft. If you have an SDR device and want to use SDRangel to generate ADS-B data for this script, follow the instructions in the [SDRangel GitHub repository](https://github.com/f4exb/sdrangel) to install and configure SDRangel for ADS-B reception.
+
+Once SDRangel is set up and receiving ADS-B messages, you can configure it to write the messages to a file in the required format. Then, you can point this script to that file to begin tracking aircraft and sending Discord notifications.
+
+Please note that the quality and quantity of ADS-B data you can receive with SDRangel will depend on your location, the quality of your SDR device and antenna, and other factors.
+
+Also, keep in mind that you can use any other source of ADS-B data, as long as the data is provided in the required format.
---
+++ adbs-streamer.py
@@ -0,0 +1,163 @@
+import time
+import requests
+import pyModeS as pms
+from datetime import datetime, timedelta
+from requests.exceptions import HTTPError
+from colorama import Fore, Style
+
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1129819369820672191/Lk0JXtAlf_115zjkFkxmwbbLm3sVuYjwrP6f5GUihbx9R4mBj1l_diN3BzdWaPSINEf5'
+FILENAME = './adsb'
+
+POSITION_MESSAGE_TIMEOUT = timedelta(minutes=2)
+MESSAGE_RATE_LIMIT = timedelta(seconds=10)
+DISCORD_RATE_LIMIT_PAUSE = 60  # Pause duration in seconds when Discord rate limit is reached
+
+class AircraftState:
+    def __init__(self):
+        self.callsign = None
+        self.altitude = 'N/A'
+        self.velocity = 'N/A'
+        self.heading = 'N/A'
+        self.lat = None
+        self.lon = None
+        self.last_position_update = None
+        self.last_even_message = None
+        self.last_even_message_t = None
+        self.last_odd_message = None
+        self.last_odd_message_t = None
+        self.last_message_time = None
+
+aircraft_states = {}
+
+def process_line(line):
+    parts = line.split(',')
+    
+    # Check that line contains at least three comma-separated fields and a timestamp
+    if len(parts) < 3 or not parts[0].strip():
+        return
+
+    try:
+        timestamp = datetime.strptime(parts[0], "%a %b %d %Y")  # adjust according to your timestamp format
+        unix_timestamp = time.mktime(timestamp.timetuple())
+        message = parts[2]
+    except IndexError:
+        return
+
+    # If message is empty or not a hexadecimal string, return
+    if not message or not all(c in '0123456789abcdefABCDEF' for c in message):
+        return
+
+    # Skip if message is not ADS-B
+    if pms.df(message) != 17:
+        return
+
+    icao = pms.adsb.icao(message)
+    tc = pms.adsb.typecode(message)
+
+    # Get aircraft state, create new if not exists
+    if icao not in aircraft_states:
+        aircraft_states[icao] = AircraftState()
+
+    state = aircraft_states[icao]
+
+    # Decode altitude, velocity, heading, and callsign if possible
+    if 5 <= tc <= 8 or 20 <= tc <= 22 or 9 <= tc <= 18:
+        try:
+            state.altitude = pms.adsb.altitude(message)
+        except RuntimeError:
+            pass
+    if tc == 19:
+        state.velocity, state.heading = pms.adsb.speed_heading(message)
+    if 1 <= tc <= 4:
+        state.callsign = pms.adsb.callsign(message)
+    if 9 <= tc <= 18:
+        if pms.adsb.oe_flag(message):
+            state.last_odd_message = message
+            state.last_odd_message_t = unix_timestamp
+        else:
+            state.last_even_message = message
+            state.last_even_message_t = unix_timestamp
+
+        if state.last_odd_message and state.last_even_message:
+            state.lat, state.lon = pms.adsb.position(state.last_even_message, state.last_odd_message, state.last_even_message_t, state.last_odd_message_t)
+            state.last_position_update = datetime.now()
+
+    # If position is too old, set to N/A
+    if state.last_position_update and datetime.now() - state.last_position_update > POSITION_MESSAGE_TIMEOUT:
+        state.lat = 'N/A'
+        state.lon = 'N/A'
+
+    # If callsign or position is not decoded, or if rate limit not passed, don't send the message
+    if state.callsign is None or state.lat is None or state.lon is None or (state.last_message_time and datetime.now() - state.last_message_time < MESSAGE_RATE_LIMIT):
+        return
+
+    decoded_data = {
+        'ICAO': icao,
+        'TypeCode': tc,
+        'Altitude': state.altitude,
+        'Velocity': state.velocity,
+        'Heading': state.heading,
+        'Callsign': state.callsign,
+        'Latitude': state.lat,
+        'Longitude': state.lon,
+    }
+
+    # Pretty print output
+    print(Fore.YELLOW + 'ICAO: ' + decoded_data['ICAO'], end=' ')
+    print(Fore.GREEN + 'TypeCode: ' + str(decoded_data['TypeCode']), end=' ')
+    print(Fore.CYAN + 'Altitude: ' + str(decoded_data['Altitude']), end=' ')
+    print(Fore.BLUE + 'Velocity: ' + str(decoded_data['Velocity']), end=' ')
+    print(Fore.MAGENTA + 'Heading: ' + str(decoded_data['Heading']), end=' ')
+    print(Fore.RED + 'Callsign: ' + decoded_data['Callsign'], end=' ')
+    print(Fore.GREEN + 'Latitude: ' + str(decoded_data['Latitude']), end=' ')
+    print(Fore.CYAN + 'Longitude: ' + str(decoded_data['Longitude']))
+    print(Style.RESET_ALL)
+
+    send_message_to_discord(decoded_data)
+
+    # Update last message time
+    state.last_message_time = datetime.now()
+
+def send_message_to_discord(decoded_data):
+    data = {
+        'content': '',
+        'embeds': [{
+            'title': 'New Aircraft Detected',
+            'description': '\n'.join(f'{k}: {v}' for k, v in decoded_data.items())
+        }]
+    }
+    while True:
+        try:
+            response = requests.post(WEBHOOK_URL, json=data)
+            response.raise_for_status()
+            break
+        except HTTPError as err:
+            if response.status_code == 429:  # Too Many Requests
+                print(Fore.RED + f"Rate limit reached, pausing for {DISCORD_RATE_LIMIT_PAUSE} seconds...")
+                print(Style.RESET_ALL)
+                time.sleep(DISCORD_RATE_LIMIT_PAUSE)
+            else:
+                print(Fore.RED + f"HTTP error occurred: {err}")
+                print(Style.RESET_ALL)
+                break
+
+def tail_file_and_process():
+    while True:
+        try:
+            with open(FILENAME, 'r') as f:
+                f.seek(0, 2)
+                while True:
+                    line = f.readline()
+                    if not line:
+                        time.sleep(0.1)
+                        continue
+                    process_line(line.strip())
+        except Exception as e:
+            print(f'Error occurred: {str(e)}, restarting...')
+            time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        tail_file_and_process()
+    except KeyboardInterrupt:
+        print('Interrupted by user, stopping.')
